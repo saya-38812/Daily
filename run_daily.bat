265085497836f1abd4a561d1f3c47799ab59d45e
saya38812 <Saya78123@gmail.com>
2026-04-18 14:44:37 +0900

    fix

--- run_daily.bat
+++ run_daily.bat
@@ -1,4 +1,9 @@
 @echo off
 cd /d "C:\Users\saya\Desktop\Daily"
+
+:: 絵文字などUnicode文字を含む予定があってもクラッシュしないようにUTF-8を強制
+set PYTHONUTF8=1
+set PYTHONIOENCODING=utf-8
+
 "C:\Python313\python.exe" main.py >> logs\run.log 2>&1
 
